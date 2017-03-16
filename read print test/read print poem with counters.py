9259723a08caf6f89e00e224c970de657e72f403
Nathan Jenkins <najenkins@student.csdspartans.org>
2017-03-16 13:58:13 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -48,7 +48,7 @@ total_lines_in_file = total_lines_in_file + 1
 
 line = file_input.readline()
 
-print('     ' + firstline + "\n" + "     " + line)
+print(" " + firstline, line)
 total_lines_in_file = total_lines_in_file + 1
 
 
@@ -119,19 +119,20 @@ while line != '':                  # while not a blank line
   else:
 
     total_lines_in_file = total_lines_in_file + 1               
-
+    song_line_counter = song_line_counter + 1
     
       
-    if total_lines_in_file < 10:
-        print(str(total_lines_in_file)+ ')   ', line, end = '')
+    if song_line_counter < 10:
+        print(str(song_line_counter)+ ')   ', line, end = '')
     else:
-      print(str(total_lines_in_file) + ')  ', line, end = '')
+      print(str(song_line_counter) + ')  ', line, end = '')
 
 
     line = file_input.readline()
 line = file_input.readline()
 print ("\n\n-- End of Song ---")
-print ("Total number of stanzas in this poem are: " + str(stanza_counter) + ".")
+print("Total number of lines in the song are: " + str(song_line_counter) + ".")
+print ("Total number of stanzas in this song are: " + str(stanza_counter) + ".")
 print ("Total number of lines in this file are: " + str(total_lines_in_file) + ".")
 print ("This song first appeared in the album 'Days of Future Passed' in 1968")
 print ("The Moody Blues members are Mike Lodge, Graeme Edge, Justin Hayward, Ray Thomas, and John Lodge.")
