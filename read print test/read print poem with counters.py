94f427eb2d418cec6fce81b11be013dcbf7a00fb
Joe Keller <jkeller@csdspartans.org>
2017-03-21 09:23:16 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -1,85 +1,39 @@
 name_of_mydocument = 'tuesdayafternoon.txt'
-
-
-
 file_input = open(name_of_mydocument, 'r')     #open file for reading
 
-
-
 total_lines_in_file = 1
-
-                                     #read the first few lines
+                                    #read the first few lines
 line = file_input.readline()
-
-
 firstline = line
-
 total_lines_in_file = total_lines_in_file + 1
 
-
 line = file_input.readline()
-
 print(" " + firstline, line)
-
 total_lines_in_file = total_lines_in_file + 1
 
-
-
 line = file_input.readline()
-
 line = file_input.readline()
-
-
 
 stanza_counter = 1
-
 song_line_counter = 0
-
-
-
 
 while line != '':                  # while not a blank line
 
-
-
-
-
-
-
   if line == "\n":
-
-
     stanza_counter = stanza_counter + 1
-
     total_lines_in_file = total_lines_in_file + 1
-
     print()
-
-
-
     line = file_input.readline()
 
-
-
   else:
-
     total_lines_in_file = total_lines_in_file + 1               
-
     song_line_counter = song_line_counter + 1
 
-    
-
-      
-
     if song_line_counter < 10:
-
         print(str(song_line_counter)+ ')   ', line, end = '')
-
     else:
 
       print(str(song_line_counter) + ')  ', line, end = '')
-
-
 
     line = file_input.readline()
 
